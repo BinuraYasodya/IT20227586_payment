5eb55617282096ef056bf66b30f03b482add5ca6
BInura Yasodya <86378121+BinuraYasodya@users.noreply.github.com>
2022-05-15 22:34:35 +0530

    exported DB

---
+++ Exported DB/it20227586_table_export.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM Payment.payments
+LIMIT 0, 1000
+
+-- Date: 2022-05-15 22:30
+*/
+INSERT INTO `` (`paymentID`,`name`,`accNumber`,`cvv`,`expDate`,`phone`,`email`) VALUES (689,'Binura','67642378472','665','04/21','0767544378','binura@gmail.com');
+INSERT INTO `` (`paymentID`,`name`,`accNumber`,`cvv`,`expDate`,`phone`,`email`) VALUES (692,'Prasan','66624868833','544','03/22','976855432','prasan@gmail.com');
+INSERT INTO `` (`paymentID`,`name`,`accNumber`,`cvv`,`expDate`,`phone`,`email`) VALUES (693,'Supun','325752395237','754','01/23','0712245376','supun@gmail.com');
+INSERT INTO `` (`paymentID`,`name`,`accNumber`,`cvv`,`expDate`,`phone`,`email`) VALUES (694,'Praveen','487967654344','322','02/11','072346789','praveen@gmail.com');
